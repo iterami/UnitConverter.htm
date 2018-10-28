26f8c7b8044340787397babc06bd13d70dcf7f51
Jan Hořava <czechinthemail@gmail.com>
2018-10-27 18:38:44 -0700

    added Dutch cask

--- js/main.js
+++ js/main.js
@@ -198,6 +198,7 @@ function repo_init(){
             'dharni [धार्नि]': 1 / 2332.5,
             'dram [dr]': 1 / 1.7718451953125,
             'dram apothecaries [dr ap]': 1 / 3.8879346,
+            'Dutch cask': 1 / 50802.34544,
             'esterling': .70548,
             'firkin': 1 / 40823.3133,
             'fun [分]': 4 / 1.5,
