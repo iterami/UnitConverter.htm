ea19256e50f89b89eaa221eba207b2ad77363f1b
Jan Hořava <czechinthemail@gmail.com>
2018-10-28 18:25:03 -0700

    fixed miles per hour squared accuracy

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function repo_init(){
             'gal [Gal]': 100,
             'metre per hour squared [m/h²]': 3600,
             'metre per second squared [m/s²]': 1,
-            'miles per hour squared [mph²]': 1 / (1.4 + 2 / 30),
+            'miles per hour squared [mph²]': 1 / .44704,
             'standard gravity [ɡ₀]': 1 / 9.80665,
           },
           'angle': {
