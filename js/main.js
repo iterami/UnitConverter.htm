f34392382eaac8e29690b7fc7a0e558116b4cb76
Jan Hořava <czechinthemail@gmail.com>
2019-01-10 15:00:44 -0800

    added fluid scruple

--- js/main.js
+++ js/main.js
@@ -471,6 +471,7 @@ function repo_init(){
             'fluid dram (US) [fl dr]': 1 / .0036966911953125,
             'fluid ounce (imperial) [fl oz]': 35.1950652,
             'fluid ounce (US) [fl oz]': 33.8140227,
+            'fluid scruple [℈]': 1 / .00123,
             'gallon (imperial) [gal]': 1 / 4.546094188,
             'gallon (US) [gal]': 1 / 3.785411784,
             'garce (India)': 1 / 5244,
