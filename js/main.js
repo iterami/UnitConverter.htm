a88c9e6e31ffd8993286551080ebb49260b51b61
Jan Hořava <czechinthemail@gmail.com>
2019-03-21 12:45:02 -0700

    added couric

--- js/main.js
+++ js/main.js
@@ -244,6 +244,7 @@ function repo_init(){
             'bundle': 1 / 317.5,
             'caret [ct]': 5,
             'cental': .0220462262185,
+            'couric': 1 / 453.59237 / 2.5,
             'cullingey': 1 / 5.265,
             'dharni [धार्नि]': 1 / 2332.5,
             'dirhem (Ottoman)': 1 / 3.20725,
