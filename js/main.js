0e856f54d331429baedba16b71c7b4b11129e486
Jan Hořava <czechinthemail@gmail.com>
2019-10-25 20:14:47 -0700

    added duella

--- js/main.js
+++ js/main.js
@@ -273,6 +273,7 @@ function repo_init(){
             'dirhem (Ottoman)': 1 / 3.20725,
             'dram [dr]': 1 / 1.7718451953125,
             'dram apothecaries [dr ap]': 1 / 3.8879346,
+            'duella': 1 / 9.056,
             'Dutch cask': 1 / 50802.34544,
             'esterling': .70548,
             'firkin': 1 / 40823.3133,
