8a1d2582f237264c8a39afe80041ffd1c02b2a1f
Jan Hořava <czechinthemail@gmail.com>
2024-01-21 16:25:16 -0800

    changed input buttons to button elements

--- repo.js
+++ repo.js
@@ -748,7 +748,7 @@ function repo_init(){
         }
 
         unittable += '<tr><td class=right><input id="' + type + '-value" step=any type=number>*10^<input class=mini id="' + type + '-input-power" step=any type=number value=0><br><select id="' + type + '-input">' + options + '</select>'
-          + '<td class=center>' + type + '<br><input id="' + type + '" type=button value=↔>'
+          + '<td class=center>' + type + '<br><button id="' + type + '" type=button>↔</button>'
           + '<td class=left><input id="' + type + '-result" readonly type=text>*10^<input class=mini id="' + type + '-output-power" step=any type=number value=0><br><select id="' + type + '-output">' + options + '</select>';
     }
     document.getElementById('units').innerHTML = unittable;
